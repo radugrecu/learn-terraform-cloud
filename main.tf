17274c78ca2d27f7ba6da5b4fd2e3c989ed9a567
Radu GRECU <radu.grecu86@gmail.com>
2022-04-14 09:39:19 +0300

    First commit

---
+++ main.tf
@@ -0,0 +1,42 @@
+terraform {
+  # cloud {
+  #   organization = "radugrecu-tutorials"
+  #   workspaces {
+  #     name = "tutorial-store-remote-state"
+  #   }
+  # }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+resource "aws_vpc" "vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "my-vpc"
+  }
+}
+resource "aws_subnet" "subnet" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.1.0/24"
+}
+
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-08d70e59c07c61a3a"
+  instance_type = "t3.micro"
+  subnet_id     = aws_subnet.subnet.id
+}
